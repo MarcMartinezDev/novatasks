9381cc8595c5438ee38cf4a0f14e581157930c12
marc martinez <marcmh1605@gmail.com>
2025-05-02 08:45:20 +0200

    add website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "http://localhost:4321",
+  site: "https://novatasks.vercel.app/",
   integrations: [sitemap()],
 });
